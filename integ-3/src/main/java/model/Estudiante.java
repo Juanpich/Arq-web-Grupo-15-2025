f7f673395aa5aa7824a9a9e037d7f2db666d5324
juanpi chiclana <juanpachiclanaurraco@gmail.com>
2025-10-13 16:14:48 -0300

    Estructura de carpetas

---
+++ integ-3/src/main/java/model/Estudiante.java
@@ -0,0 +1,32 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Setter
+@Entity
+public class Estudiante {
+    @Id
+    private int dni;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String genero;
+    @Column
+    private int edad;
+    @Column
+    private String ciudad;
+    @Column
+    private int LU;
+    @OneToMany(mappedBy = "id_estudiante")
+    private List<EstudianteCarrera> carreras;
+}
