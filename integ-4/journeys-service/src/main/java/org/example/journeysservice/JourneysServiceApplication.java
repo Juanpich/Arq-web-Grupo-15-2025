20db35405cdadf21db0b46801b8b59ec430bb7b2
juanpi chiclana <juanpachiclanaurraco@gmail.com>
2025-11-03 16:20:51 -0300

    Crear proyecto del integ-4 Spring

---
+++ integ-4/journeys-service/src/main/java/org/example/journeysservice/JourneysServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.journeysservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneysServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneysServiceApplication.class, args);
+    }
+
+}
